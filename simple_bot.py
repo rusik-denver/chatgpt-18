b61d493cefca63be2a04b116b9eac7acc7efecfe
rusik.denver <rusik.denver@gmail.com>
2024-05-12 23:23:45 +0300

    history added

--- simple_bot.py
+++ simple_bot.py
@@ -39,14 +39,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # при первом запуске бота добавляем этого пользователя в словарь
     if update.message.from_user.id not in context.bot_data.keys():
-        context.bot_data[update.message.from_user.id] = 3
+        context.bot_data[update.message.from_user.id] = {'limit': 3}
     
     # возвращаем текстовое сообщение пользователю
     await update.message.reply_text('Задайте любой вопрос ChatGPT')
 
 #функция-обработчик команды /status
 async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'Кол-во доступных запросов: {context.bot_data[update.message.from_user.id]}')
+    await update.message.reply_text(f'Кол-во доступных запросов: {context.bot_data[update.message.from_user.id]['limit']}')
 
 # функция-обработчик команды /data 
 async def data(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -58,13 +58,18 @@ async def data(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # возвращаем текстовое сообщение пользователю
     await update.message.reply_text('Данные сгружены')
 
+# функция-обработчик команды /history 
+async def history(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.from_user.id in context.bot_data.keys():
+        await update.message.reply_text(f'История запросов пользователя {update.message.from_user.first_name}:\n')
+
+        for row in context.bot_data[update.message.from_user.id]['history'].items():
+            await update.message.reply_text(f'Запрос №{row[0]}:\nВопрос: {row[1]['request']}\n{row[1]['reply']}\n\n\n')
 
 # функция-обработчик текстовых сообщений
 async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     # проверка доступных запросов пользователя
-    if context.bot_data[update.message.from_user.id] > 0:
-
+    if context.bot_data[update.message.from_user.id]['limit'] > 0:
         # выполнение запроса в chatgpt
         first_message = await update.message.reply_text('Ваш запрос обрабатывается, пожалуйста подождите...')
         # res = await get_answer(update.message.text)
@@ -72,7 +77,25 @@ async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.edit_message_text(text=res['message'], chat_id=update.message.chat_id, message_id=first_message.message_id)
 
         # уменьшаем количество доступных запросов на 1
-        context.bot_data[update.message.from_user.id]-=1
+        context.bot_data[update.message.from_user.id]['limit']-=1
+
+        if 'counter' in context.bot_data[update.message.from_user.id].keys():
+            context.bot_data[update.message.from_user.id]['counter'] += 1
+        else:
+            context.bot_data[update.message.from_user.id].update({'counter' :0})
+
+        counter = context.bot_data[update.message.from_user.id]['counter']
+
+        if counter > 4:
+            counter = 0
+
+        if 'history' in context.bot_data[update.message.from_user.id].keys():
+            context.bot_data[update.message.from_user.id]['history'][counter] = {'request': update.message.text,'reply': res['message']}
+        else:
+            context.bot_data[update.message.from_user.id].update({'history' : {counter: {'request': update.message.text,'reply': res['message']}}})
+
+        with open('data.json', 'w') as fp:
+            json.dump(context.bot_data, fp)
         # await update.message.reply_text(f'Осталось запросов: {context.bot_data[update.message.from_user.id]}')
     
     else:
@@ -89,7 +112,8 @@ async def callback_daily(context: ContextTypes.DEFAULT_TYPE):
 
         # проходим по всем пользователям в базе и обновляем их доступные запросы
         for key in context.bot_data:
-            context.bot_data[key] = 5
+            context.bot_data[key] = context.bot_data[key]
+            context.bot_data[key]['limit'] = 5
         print('Запросы пользователей обновлены')
     else:
         print('Не найдено ни одного пользователя')
@@ -111,6 +135,7 @@ def main():
     application.add_handler(CommandHandler("start", start, block=True))
     application.add_handler(CommandHandler("status", status, block=True))
     application.add_handler(CommandHandler("data", data, block=True))
+    application.add_handler(CommandHandler("history", history, block=True))
     application.add_handler(MessageHandler(filters.TEXT, text, block=True))
 
     # запуск бота (нажать Ctrl+C для остановки)
